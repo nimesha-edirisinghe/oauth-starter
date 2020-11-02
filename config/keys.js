4aa9b4b1a82e3ccc1d484ff75013bae46a2b152a
Nimesha Edirisinghe <57697012+nimesha-edirisinghe@users.noreply.github.com>
2020-11-02 10:04:37 +0530

    update key file

--- config/keys.js
+++ config/keys.js
@@ -3,14 +3,14 @@
 module.exports = {
   google: {
     clientID:
-      "829080903172-27o99kaqthashr00ljj7jgbqeborshns.apps.googleusercontent.com",
-    clientSecret: "Kg8_Gyzg0CEyJe4r_SpZUjQS",
+      "",
+    clientSecret: "",
   },
   mongodb: {
     dbURI:
-      "mongodb+srv://nim123:nim123@nimcluster.vtjdf.mongodb.net/oauth-db?retryWrites=true&w=majority",
+      "",
   },
   session: {
-    cookieKey: "thenetninjaisawesomeiguess",
+    cookieKey: "",
   },
 };
